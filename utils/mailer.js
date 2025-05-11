41133c9221e6fa11930b4215bc53df926bd4eb3b
Ista07-ui <i.c.k.85995@gmail.com>
2025-05-11 21:00:32 +0700

    tambah email sending

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,7 +2,9 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail", // or your SMTP
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -12,10 +14,17 @@ export const transporter = nodemailer.createTransport({
 export const sendVerificationEmail = async (email, token) => {
   const verificationUrl = `http://localhost:3000/api/auth/verify?token=${token}`;
 
+  await transporter.verify();
+  console.log("Server is ready to take our messages");
+
   await transporter.sendMail({
     from: `"Course App" <${process.env.EMAIL_USER}>`,
     to: email,
     subject: "Verify your email",
     html: `<p>Please click <a href="${verificationUrl}">this link</a> to verify your email.</p>`,
   });
+
+  console.log("Preview URL: https:ethereal.email");
+  console.log("username :", process.env.EMAIL_USER);
+  console.log("password :", process.env.EMAIL_PASS);
 };
